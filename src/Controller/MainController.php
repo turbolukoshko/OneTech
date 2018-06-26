4b4c4d01d821c572fe08b83e566ee41bfd4c1698
Vladislav <turbo.lukoshko@gmail.com>
2018-06-26 03:52:01 +0300

    added twig templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,4 +16,74 @@ class MainController extends Controller
             'page_title' => 'OneTech',
         ]);
     }
+
+    /**
+     * @Route("/shop", name="shop")
+     */
+    public function shop()
+    {
+        return $this->render('shop/shop.html.twig', [
+            'page_title' => 'Shop',
+        ]);
+    }
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function product()
+    {
+        return $this->render('product/product.html.twig', [
+            'page_title' => 'Single Product',
+        ]);
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blog()
+    {
+        return $this->render('blog/blog.html.twig', [
+            'page_title' => 'Blog',
+        ]);
+    }
+
+    /**
+     * @Route("/blog_single", name="blog_single")
+     */
+    public function blogSingle()
+    {
+        return $this->render('blog_post/blog_single.html.twig', [
+            'page_title' => 'Blog Post',
+        ]);
+    }
+
+    /**
+     * @Route("/regular", name="regular")
+     */
+    public function regularPost()
+    {
+        return $this->render('regular_post/regular_post.html.twig', [
+            'page_title' => 'Regular Static',
+        ]);
+    }
+
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function cart()
+    {
+        return $this->render('cart/cart.html.twig', [
+            'page_title' => 'Cart',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact/contact.html.twig', [
+            'page_title' => 'Contact',
+        ]);
+    }
 }
